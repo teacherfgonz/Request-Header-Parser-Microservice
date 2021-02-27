eb3c4a006927652760e0abcb2af0cdcaa2e4823f
teacherfgonz <teacherfranciscogonz@gmail.comgit config --global --listgit config --global user.email teacherfranciscogonz@gmail.com>
2021-02-27 20:56:37 -0300

    whoami updated

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -25,9 +26,16 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami/", function(req, res) {
+  res.json({
+    "ip": req.ip,
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  })
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
